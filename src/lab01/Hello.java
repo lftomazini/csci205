888c72bcc8009e0f85225fad149098535b47251b
Luis Felipe Tomazini <lffct001@bucknell.edu>
2015-08-25 23:31:45 -0400

    Lab01, Exercise 3, Complete

---
+++ src/lab01/Hello.java
@@ -0,0 +1,23 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Fall 2015
+* Instructor: Brian King
+* Section: 10am
+*
+* Name: Luis Felipe Franco Candeo Tomazini
+* Date: 08/25/2015
+*
+* Lab / Assignment: Lab01
+*
+* Description: Hello World
+*
+* *****************************************/
+
+package lab01;
+public class Hello
+{
+ public static void main( String[] args ){
+ System.out.println("Programming is not a spectator sport!");
+ System.exit(0);
+ }
+}
