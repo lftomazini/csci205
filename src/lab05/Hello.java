a05438f82804e70b994179727ce964ad880143a0
Luis Felipe Tomazini <lffct001@bucknell.edu>
2015-09-07 13:59:05 -0400

    Lab05, Exercise 1, Complete

--- src/lab05/Hello.java
+++ src/lab05/Hello.java
@@ -15,6 +15,10 @@
  */
 package lab05;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A simple program to aid in understanding NetBeans
  *
@@ -23,13 +27,26 @@ package lab05;
  */
 public class Hello {
 
+    private static final int NUM_INT = 10;
+
     /**
      * Prints an alternative "hello, World!"
      *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Becoming a dood programmer takes practice.");
+        System.out.print("Hello. What is your name? ");
+        Scanner in = new Scanner(System.in);
+        String name = in.next();
+        System.out.println(name + ", Becoming a dood programmer takes practice.");
+
+        // Some simple code to test the debugger
+        Random rand = new Random();
+        int[] x = new int[NUM_INT];
+        for (int i = 0; i < NUM_INT; i++) {
+            x[i] = rand.nextInt(100);
+        }
+        System.out.println(Arrays.toString(x));
     }
 
 }
