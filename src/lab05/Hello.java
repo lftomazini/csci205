bf51e6e6bed1c4bf47203eefe98d3780751ac9d5
Luis Felipe Tomazini <lffct001@bucknell.edu>
2015-09-07 10:35:16 -0400

    Lab05, Exercise 1, a simple Hello World program

---
+++ src/lab05/Hello.java
@@ -0,0 +1,35 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2015
+ *
+ * Name: Luis Felipe Franco Candeo Tomazini
+ * Date: Sep 7, 2015
+ * Time: 10:23:47 AM
+ *
+ * Project: csci205
+ * Package: lab05
+ * File: Hello
+ * Description:
+ *
+ * ****************************************
+ */
+package lab05;
+
+/**
+ * A simple program to aid in understanding NetBeans
+ *
+ * @author Luis Felipe Tomazini
+ * @version 0.1
+ */
+public class Hello {
+
+    /**
+     * Prints an alternative "hello, World!"
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Becoming a dood programmer takes practice.");
+    }
+
+}
