c812273d134e8f973bb67f3416e8cef8c2d5e1dd
Luis Felipe Tomazini <lffct001@bucknell.edu>
2015-09-16 21:20:36 -0400

    Lab08, Exercise 1, Complete

---
+++ src/lab08/Coin.java
@@ -0,0 +1,25 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2015
+ *
+ * Name: Luis Felipe Franco Candeo Tomazini
+ * Date: Sep 16, 2015
+ * Time: 8:24:41 PM
+ *
+ * Project: csci205
+ * Package: lab08
+ * File: Coin
+ * Description:
+ *
+ * ****************************************
+ */
+package lab08;
+
+/**
+ *
+ * @author lffct001
+ */
+public enum Coin {
+    NICKEL, DIME, QUARTER;
+
+}
